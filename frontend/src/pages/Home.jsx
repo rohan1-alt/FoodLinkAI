d9452768ded5f27f645938378d0314246e29c2bb
AnushreeKasturi <anushree.kasturi06@gmail.com>
2026-07-27 19:46:44 +0530

    completed home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import HowItWorks from "../components/HowItWorks";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <HowItWorks />
     </>
   );
 }
